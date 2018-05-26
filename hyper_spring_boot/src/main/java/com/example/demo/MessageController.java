07ffd9524820a861f0185b5c90533374c113dad6
sonshi <graveyard_000000000@yahoo.co.jp>
2018-05-27 01:43:40 +0900

    成果物

---
+++ hyper_spring_boot/src/main/java/com/example/demo/MessageController.java
@@ -0,0 +1,41 @@
+package com.example.demo;
+
+import java.util.List;
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class MessageController {
+	
+	@Autowired
+	private MessageService service;
+	
+	@GetMapping("/messages")
+	public String messages(Model model) {
+		model.addAttribute("messageForm", new MessageForm());
+		
+		List<Message>messages = service.getRecentMessages(100);
+		model.addAttribute("messages", messages);
+		
+		return "messages";
+	}
+	
+	@PostMapping("/messages")
+	public String messagePost(Model model, @Valid MessageForm messageForm, BindingResult bindingResult, HttpServletRequest request) {
+		if (bindingResult.hasErrors()) {
+			List<Message>messages = service.getRecentMessages(100);
+			model.addAttribute("messages", messages);
+			return "messages";
+		}
+	
+		service.save(new Message(messageForm.getName(), messageForm.getText(), request.getRemoteAddr()));
+		return "redirect:/messages";
+	}
+}
---
+++ hyper_spring_boot/src/main/java/com/example/demo/MessageRepository.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.CrudRepository;
+
+@Repository
+public interface MessageRepository extends CrudRepository<Message, Long>{
+	
+	List<Message> findByOrderByIdDesc(Pageable pageable);
+	
+}
