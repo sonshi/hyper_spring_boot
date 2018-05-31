c746947fa44b8b0d1cad8e65d20b72dd575a29d8
sonshi <graveyard_000000000@yahoo.co.jp>
2018-05-31 22:39:18 +0900

    Revert "HelloWorld_Web_SpringBoot"
    This reverts commit d3d6036439abaddfcb8533b4f92aa1773ccbe1a3.

--- hyper_spring_boot/src/main/java/com/example/demo/MessageController.java
+++ hyper_spring_boot/src/main/java/com/example/demo/MessageController.java
@@ -11,7 +11,6 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
-
 @Controller
 public class MessageController {
 	
